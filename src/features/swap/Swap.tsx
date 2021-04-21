8576bb657a541b3453925a43e35f5cfea738a43d
Sigit Kuncoro <sigit.kunc@gmail.com>
2021-04-21 18:44:08 +0700

    feat: init tailwindcss and antd design UI

---
+++ src/features/swap/Swap.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Swap = (props: any) => {
+  return <div>Hallo</div>;
+};
+
+export default Swap;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
 
-import Counter from '../features/counter/Counter'
-import styles from '../styles/Home.module.css'
+// import Counter from "../features/counter/Counter";
+import Swap from "../features/swap/Swap";
+import styles from "../styles/Home.module.css";
 
 const IndexPage: NextPage = () => {
   return (
@@ -12,52 +13,11 @@ const IndexPage: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <img src="/logo.svg" className={styles.logo} alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className={styles.link}
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className={styles.link}
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        {/* <Counter /> */}
+        <Swap />
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
